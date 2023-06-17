d2a2a297d380ad1b62acb773442d6589e819d268
Wilson Doonan <chanceboardgames@gmail.com>
2023-06-17 18:43:06 +0000

    2

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://kit.fontawesome.com/536dad5a0d.js" crossorigin="anonymous"></script>
+        <link rel='icon' href='favicon.svg' />
+        <title>Coding Stock</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- website/pages/index.js
+++ website/pages/index.js
@@ -3,13 +3,30 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+function Review({Text}) {
+  return (
+    <div className='col-span-12 sm:col-span-6 md:col-span-4 rounded-xl text-slate-200'>
+      <div className='text-2xl text-center m-12'>
+        ⭐⭐⭐⭐⭐
+      </div>
+      <div className='text-center text-4xl m-12 mt-0'>
+        {Text}
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <body className='bg-gradient-to-br from-slate-900 to-slate-700 flex w-screen h-screen text-slate-200'>
-      <div className='w-full'>
-        <div className='mx-auto my-12'>
-          <h1 className='text-5xl underline decoration-violet-900 text-center'>Coding Stock</h1>
-        </div>        
+    <body className='bg-gradient-to-br from-slate-900 to-slate-700 w-screen h-screen text-slate-200'>
+      <div className='w-4/5 mx-auto gap-4 grid columns-12'>
+        <div className='col-span-12 mx-auto my-36'>
+          <h1 className='text-5xl md:text-8xl underline decoration-violet-900 text-center'>Coding Stock <i className="fa-light fa-plus"></i></h1>
+          <p className='text-2xl m-5 text-center'>Build With Confidence</p>
+        </div>
+        <Review Text='No better way to find and host assets!'/>
+        <Review Text='It was just the tool I needed!'/>
+        <Review Text='I have been blown away with the speed!'/>    
       </div>
     </body>
   )
